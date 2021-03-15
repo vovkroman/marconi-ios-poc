3f313c735dc08e20895ad5bebd87b9d5aaf47910
Roman Vovk <roman.vovk@globallogic.com>
2021-03-15 18:45:16 +0200

    ios-marconi-poc: ios-marconi-framework.framework has been added to .workspace

---
+++ ios-marconi-framework/ios-marconi-framework/ios_marconi_framework.h
@@ -0,0 +1,19 @@
+//
+//  ios_marconi_framework.h
+//  ios-marconi-framework
+//
+//  Created by Roman Vovk on 15.03.2021.
+//  Copyright © 2021 Roman Vovk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ios_marconi_framework.
+FOUNDATION_EXPORT double ios_marconi_frameworkVersionNumber;
+
+//! Project version string for ios_marconi_framework.
+FOUNDATION_EXPORT const unsigned char ios_marconi_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_marconi_framework/PublicHeader.h>
+
+
