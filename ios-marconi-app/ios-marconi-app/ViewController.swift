fa14b1b2ff5e7943df5eda9e83a71471cc710006
Roman Vovk <roman.vovk@globallogic.com>
2021-03-15 18:46:52 +0200

    ios-marconi-poc: ios-marconi-app has been added to .workspace

---
+++ ios-marconi-app/ios-marconi-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-marconi-app
+//
+//  Created by Roman Vovk on 15.03.2021.
+//  Copyright © 2021 Roman Vovk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios-marconi-app/ios-marconi-appTests/ios_marconi_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_marconi_appTests.swift
+//  ios-marconi-appTests
+//
+//  Created by Roman Vovk on 15.03.2021.
+//  Copyright © 2021 Roman Vovk. All rights reserved.
+//
+
+import XCTest
+@testable import ios_marconi_app
+
+class ios_marconi_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
