c7a8677e694964274ccbcf412c847966702c6bcd
Roman Vovk <roman.vovk@globallogic.com>
2021-03-18 14:51:54 +0200

    SA-722: stup UI for demo app (+ added some NibLoadable protocols for UI reusing)

---
+++ ios-marconi-app/ios-marconi-app/UI/Extensions/Reusable/Reusable.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+protocol Reusable: class {
+    static var reuseIdentifier: String { get }
+}
+
+extension Reusable {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+typealias NibReusable = Reusable & NibLoadable
