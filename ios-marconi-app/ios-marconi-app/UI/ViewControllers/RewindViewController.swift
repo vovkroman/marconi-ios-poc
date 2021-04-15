6415769c8236cd0413640c2fd0e1f68c499ac344
Roman Vovk <roman.vovk@globallogic.com>
2021-04-15 15:28:32 +0300

    ios-marconi-poc: added placeholders (ViewController, ViewModel) for rewind stations

---
+++ ios-marconi-app/ios-marconi-app/UI/ViewControllers/RewindViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RewindViewController.swift
+//  ios-marconi-app
+//
+//  Created by Roman Vovk on 15.04.2021.
+//  Copyright © 2021 Roman Vovk. All rights reserved.
+//
+
+import UIKit
+
+extension Rewind {
+    class ViewController: ListViewController<ViewModel> {}
+}
+
